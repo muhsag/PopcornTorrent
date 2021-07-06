aa997515303fa3f533c69e15ce9678dd97128391
Alexandru Tudose <alex.tudose@tapptitude.com>
2021-07-06 11:06:54 +0300

    Headers

---
+++ PopcornTorrent/Source/include/PopcornTorrent.h
@@ -0,0 +1,8 @@
+
+#import "PTTorrentStreamer.h"
+#import "PTTorrentDownload.h"
+#import "PTTorrentDownloadManager.h"
+#import "PTSize.h"
+#import "PTTorrentDownloadStatus.h"
+#import "PTTorrentStatus.h"
+#import "PTTorrentDownloadManagerListener.h"
